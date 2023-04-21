29048664f07b56f53281dde34f939cd3c2189ff0
Cesar Bastidas <abastidas050@gmail.com>
2023-04-21 19:42:38 -0400

    Tarea 4 cesar

---
+++ M03 JavaScript Fundamentos/operadoresLogicos.js
@@ -0,0 +1,9 @@
+function condicionalCompleja(num){
+    if(num > 9 && num % 2 === 0 || num === 3) console.log(true);
+    else console.log(false);
+}
+
+condicionalCompleja(10);
+condicionalCompleja(6);
+condicionalCompleja(3);
+condicionalCompleja(5);
---
+++ M03 JavaScript Fundamentos/test/bucles.js
@@ -0,0 +1,18 @@
+while (suma < 3){
+    suma = suma + 1;
+    console.log(suma);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
