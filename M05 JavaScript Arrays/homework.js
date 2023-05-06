69bc9ce073201b329af3fdaa557468a09a89dc3b
Cesar Bastidas <abastidas050@gmail.com>
2023-05-05 20:42:29 -0400

    Tarea 5

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -22,12 +22,18 @@ function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   for (let i = 0; i < array.length; i++) {
+        array[i] = array[i] + 1;
+      
+   }
+   return array;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   return (elemento, array).push;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
